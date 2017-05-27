ca45a7c63432336760c0c2ba0ad9b18f9bef9243
GrapeSoftCN <jk282@163.com>
2017-05-27 09:45:27 +0800

    运行单位管理

---
+++ src/test/java/test/testPerson.java
@@ -0,0 +1,9 @@
+package test;
+
+import interfaceApplication.personal;
+
+public class testPerson {
+	public static void main(String[] args) {
+		System.out.println(new personal().PersonPageBy(1, 5, "{\"ownid\":0}"));
+	}
+}
